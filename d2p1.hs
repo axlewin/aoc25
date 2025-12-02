6b1c8f8ba1052ea193ab8ffb209d95a4f996aa30
Alex Lewin <sl2281@cam.ac.uk>
2025-12-02 19:26:24 +0000

    uno

---
+++ d2p1.hs
@@ -0,0 +1,38 @@
+import System.IO
+import Data.List
+import Data.Char
+
+main :: IO ()
+main = do
+    contents <- readFile "d2.txt"
+    let 
+        ranges = readInput contents
+        bad = concatMap (filter isBad . expandRange) ranges
+    print (sum bad)
+
+readRange :: String -> (Int, Int)
+readRange s =
+    let (a, b) = break (== '-') s
+    in (read a, read (tail b))
+
+readInput :: String -> [(Int, Int)]
+readInput = map readRange . split ',' . filter (/= '\n')
+
+expandRange :: (Int, Int) -> [Int]
+expandRange (a, b) = [a .. b]
+
+isBad :: Int -> Bool
+isBad n =
+    let s = show n
+        len = length s
+    in  even len &&
+        let (h1, h2) = splitAt (div len 2) s
+        in h1 == h2
+
+split :: Eq a => a -> [a] -> [[a]]
+split _ [] = []
+split d xs =
+    let (h, t) = break (== d) xs
+    in h : case t of
+              [] -> []
+              (_:rest) -> split d rest
